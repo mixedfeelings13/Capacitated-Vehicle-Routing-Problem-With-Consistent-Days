5d0110ca521497c560f781c8df173bf4db487e74
Antonella G <fuegodjh@gmail.com>
2023-05-18 23:04:58 +0100

    Fixed bugs, added graph for each day

--- cvrptw.jl
+++ cvrptw.jl
@@ -9,9 +9,9 @@ using Colors
 Random.seed!()
 
 
-n = 7 # número de clientes
+n = 15 # número de clientes
 Q = 8 # capacidad de los vehículos
-K = 2 # número de vehículos
+K = 5 # número de vehículos
 M = 1000 # Valor grande para la restricción
 # Variables para limpiar
 C = 1:n # 1 a n = clientes
@@ -23,9 +23,6 @@ demands = [[rand(0:8) for i in C] for d in days]
 # println(d)
 # println(demands)
 # si imprime 3 demandas para 3 dias de 15 clientes
-
-# Tiempo para realizar el servicio aleatorio
-service_times = [[rand(1:5) for i in C] for d in days]
 
 # Ventanas de tiempo aleatorias consistentes
 earliest_start = [[rand(0:10) for i in C] for d in days]
@@ -53,28 +50,31 @@ model = Model(Gurobi.Optimizer)
 
 # Variable de decisión
 @variable(model, x[C, C, V, days], Bin) # Decisión 0 o 1 para x, vehículo va o no va a un cliente
-@variable(model, arrival_time[C,days] >= 0)
+@variable(model, arrival_time[C, days] >= 0)
+@variable(model, load[C, days])
+
 
 # Función objetivo
 @objective(model, Min, sum(c[i, j] * x[i, j, k, d] for i in C, j in C, k in V, d in days))
 
 # Restricción de visita única para cada cliente
 @constraint(model, visit_once[i in C], sum(x[i, j, k, d] for j in C, d in days, k in V) == 1)
-@constraint(model, exit_once[i in C], sum(x[i, j, k, d] for j in C, d in days, k in V) == 1)
+@constraint(model, exit_once[i in C], sum(x[j, i, k, d] for j in C, d in days, k in V) == 1)
 # Restricción de capacidad para cada vehículo
-@constraint(model, capacity[k in V, d in days], sum(demands[d][i] * x[i, j, k, d] for i in C, j in C) <= Q)
+@constraint(model, capacity[k in V, d in days], sum(demands[d][i] * x[i, j, k, d] for i in C, j in C) <= Q) 
 # Restricción de ventana de tiempo consistente
 for d in days
     for i in C
-        @constraint(model, sum(service_times[d][i]*x[i, j, k, d] for j in C, k in V) + arrival_time[i,d] <= latest_end[d][i])
-        @constraint(model, sum(service_times[d][i]*x[i, j, k, d] for j in C, k in V) + arrival_time[i,d] >= earliest_start[d][i])
+        @constraint(model, sum(x[i, j, k, d] for j in C, k in V) + arrival_time[i,d] <= latest_end[d][i])
+        @constraint(model, sum(x[i, j, k, d] for j in C, k in V) + arrival_time[i,d] >= earliest_start[d][i])
     end
 end
+# Restricción de los días
 for d in days
     for i in C
         for j in C
-            for k in V
-                @constraint(model, arrival_time[j, d] >= arrival_time[i, d] + demands[d][i] - M * (1 - x[i, j, k, d]))
+            for k in V 
+                @constraint(model, load[j, d] >= load[i, d] + demands[d][i] * x[i, j, k, d] - M * (1 - x[i, j, k, d]))
             end
         end
     end
@@ -109,42 +109,50 @@ for k in V
         elseif(length(join(string.(route), ", ")) < 6)         
             tabs="\t\t"
         end
-        # l = length(join(string.(route), ", "));
-        # print("S:$l")
         print("$route$tabs|\t")
     end
     print("\n")
 end
 println()
 
-# Definir la información en plots
-routes =  Dict{Int, Vector{Int}}()
+routes = Dict{Int, Dict{Int, Vector{Int}}}()
 
 for d in days
-    route = [i for i in C if any(solution[i, j, k, d] > 0.5 for j in C, k in V)]
-    routes[d] = route
+    routes[d] = Dict{Int, Vector{Int}}()
+    for k in V
+        route = [i for i in C if any(solution[i, j, k, d] > 0.5 for j in C)]
+        routes[d][k] = route
+    end
 end
 
 # Mostrar el gráfico
-p = plot()
-
-title!("Capacitated Vehicle Routing Problem With Consistent Days")
-# Nodos de clientes en color rosa
-scatter!([coords[i][1] for i in C], [coords[i][2] for i in C], label = "Clients", color = :hotpink, markersize = 15, legend = :topleft)
-
-# define las coordenadas de las rutas
-pointsArray = []
-#Añado el deposito visiblemente
-scatter!([10], [12], label = "Depot", color = :red, markersize = 20, legend = :topleft)
-#Cambio el tamaño para que sea más visible
-plot!(size=(2040,1080))
-# Creo lineas para las rutas que sean visibles.
 for d in days
-    route = routes[d]
-    coordsRoute = [coords[i] for i in route]
-    pushfirst!(coordsRoute, (10,12))
-    push!(coordsRoute, (10,12))
-    plot!(coordsRoute, label = "Day $d, Route $d", arrow=(:closed, 2.0),linewidth = 5, legend = :topleft, palette = :rainbow)
+    local p = plot()
+    title!("Capacitated Vehicle Routing Problem With Consistent Days. Day $d")
+    # Nodos de clientes en color rosa
+    scatter!([coords[i][1] for i in C], [coords[i][2] for i in C], label = "Clients", color = :hotpink, markersize = 15, legend = :topleft)
+
+    # define las coordenadas de las rutas
+    local pointsArray = []
+    #Añado el deposito visiblemente
+    scatter!([10], [12], label = "Depot", color = :red, markersize = 20, legend = :topleft)
+    #Cambio el tamaño para que sea más visible
+    plot!(size=(2040,1080))
+
+    # Creo lineas para las rutas que sean visibles.
+    for k in V
+        route = routes[d][k]
+        coordsRoute = [coords[i] for i in route]
+        ## If coordsRoute is empty, continue
+        if(isempty(coordsRoute)) 
+            continue
+        end
+
+        pushfirst!(coordsRoute, (10,12))
+        push!(coordsRoute, (10,12))
+
+        plot!(coordsRoute, label =  "Route $d, Vehicle $k", arrow=(:closed, 2.0),linewidth = 5, legend = :topleft, palette = :rainbow)
+    end
+    display(p)
+    savefig("Solution_Figure_Day_$d.png")
 end
-display(p)
-savefig("Solution_Figure.pdf")
