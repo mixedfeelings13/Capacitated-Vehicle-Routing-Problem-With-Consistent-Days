24970f37d80f5d2c626ae8189482bf3a74504f58
Antonella G <fuegodjh@gmail.com>
2023-06-15 11:04:40 +0100

    committed

--- cvrptc.jl
+++ cvrptc.jl
@@ -36,8 +36,10 @@ pushfirst!(coords, (11,13)) # Coordenadas del deposito
 # println(coords)
 distances = [Euclidean()(coords[i], coords[j]) for i in locations, j in locations] # Distancias entre clientes
 
-demands = [rand(1:100) < Pd ? rand(1:client_demand) : 0 for i in clients, d in days] # Demanda de cada cliente en cada dia<
-# println(demands)
+# Demands goes from 2 to clients 
+demands = [rand(0:client_demand) for i in locations, j in days] # Demanda de cada cliente en cada dia
+println(demands)
+
 # Creamos el modelo
 model = Model(Gurobi.Optimizer)
 
@@ -57,7 +59,7 @@ model = Model(Gurobi.Optimizer)
 # Para cada cliente y dia la suma de los vehiculos que llegan al cliente debe ser igual a 1
 for i in clients
     for d in days
-        if (demands[i-1,d] >= 1)
+        if (demands[i,d] >= 1)
             @constraint(model, sum(x[i, j, d] for j in locations) == 1)
             @constraint(model, sum(x[j, i, d] for j in locations) == 1)
         else 
@@ -66,16 +68,14 @@ for i in clients
 end
 
 # Restriccion de carga
-@constraint(model, [i in clients, j in clients, d in days],  load[j,d] >= load[i,d] + demands[i-1,d] * x[i, j, d] - Md * (1 - x[i, j, d]))
+@constraint(model, [i in clients, j in clients, d in days],  load[j,d] >= load[i,d] + demands[i,d] * x[i, j, d] - Md * (1 - x[i, j, d]))
 # Restricción de tiempo de llegada
 @constraint(model, [i in locations, j in clients, d in days],  time[j,d] >= time[i,d] + distances[i,j] * x[i, j, d] - Mt * (1 - x[i, j, d]))
 
 # Restricción de tiempo
 for i in clients,  d in days, e in days
     if (d == e) continue end
-    if (demands[i-1,d] >= 1 && demands[i-1,e] >= 1)
-        @constraint(model, time[i,d] - time[i,e] <= L) # La diferencia de tiempo de espera del cliente i en el dia d y el dia anterior debe ser menor que L
-    end
+    @constraint(model, time[i,d] - time[i,e] <= L) # La diferencia de tiempo de espera del cliente i en el dia d y el dia anterior debe ser menor que L
 end
 
 # Cada vehiculo debe empezar y terminar en el deposito
@@ -142,7 +142,6 @@ for d in days
         end
         plot!(coordsRoute, label =  "Route", arrow=(:closed, 2.0), linewidth = 5, legend = :outertopright, palette = palette(:Set2))
 
-    
     # Creo lineas para las rutas que sean visibles.
     # for k in vehicles
     #     route = routes[d][k]
